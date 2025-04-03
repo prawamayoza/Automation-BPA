369effe29150b323003049dbff0901373fecb7a7
prawamayoza <prawamayoza@gmail.com>
2025-04-03 20:27:15 +0700

    work order

---
+++ cypress/e2e/work order/read-wo.cy.js
@@ -0,0 +1,18 @@
+describe('read page work order', () => {
+    it('should display of data work order', () => {
+        cy.contains('.css-1m6080r', 'Fulfillment')
+        .scrollIntoView()
+        .should('be.visible')
+        .wait(500)
+        .click();
+        // Tunggu hingga halaman Fulfillment benar-benar muncul
+        cy.url().should('include', 'https://bpa-master.girudo.id/fulfillment/delivery-order');  // Pastikan URL berubah
+        cy.contains('p.chakra-text.css-tzqku2', 'fulfillment', { timeout: 10000 }).should('be.visible');
+        cy.get('.css-s9z86k')
+        .should('contain.text', 'Work Order') // Pastikan teks ada
+        .contains('Work Order')
+        .click();
+        cy.url().should('include', '/fulfillment/work-order'); 
+
+    });
+});
