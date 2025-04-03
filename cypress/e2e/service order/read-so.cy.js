a8dd2df3baef338db29da1f5324ee44e7859581d
prawamayoza <prawamayoza@gmail.com>
2025-04-03 20:26:39 +0700

    Service Order

---
+++ cypress/e2e/service order/read-so.cy.js
@@ -0,0 +1,18 @@
+describe('read page service order', () => {
+    it('should display of data service order', () => {
+        cy.contains('.css-1m6080r', 'Fulfillment')
+        .scrollIntoView()
+        .should('be.visible')
+        .wait(500)
+        .click();
+        // Tunggu hingga halaman Fulfillment benar-benar muncul
+        cy.url().should('include', 'https://bpa-master.girudo.id/fulfillment/delivery-order');  // Pastikan URL berubah
+        cy.contains('p.chakra-text.css-tzqku2', 'fulfillment', { timeout: 10000 }).should('be.visible');
+        cy.get('.css-s9z86k')
+        .should('contain.text', 'Service Order') // Pastikan teks ada
+        .contains('Service Order')
+        .click();
+        cy.url().should('include', '/fulfillment/service-order'); 
+
+    });
+});
